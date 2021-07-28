2e72399f8aa84752f3b72e22a65bed3cfe4818f6
alobatonavantica <alvaro.lobaton@avantica.com>
2021-07-28 11:50:23 -0500

    Spring initializr

---
+++ src/test/java/com/co/kafkapoc/KafkaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.kafkapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
